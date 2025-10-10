18043f1cb8e49049bfe65c72329f37fbadc369c7
Clay Shumaker <clayalshu@gmail.com>
2025-10-10 11:37:16 -0700

    added accept message

--- lurk.lua
+++ lurk.lua
@@ -1,6 +1,6 @@
-lurk = Proto("lurk", "LURK")
+local lurk = Proto("lurk", "LURK")
 
-local lurk_type = ProtoField.uint8("lurk.msg_type", "Message Type", base.HEX, {
+local message_types = {
     [0x01] = "Message",
     [0x02] = "Change Room",
     [0x03] = "Fight",
@@ -15,9 +15,56 @@ local lurk_type = ProtoField.uint8("lurk.msg_type", "Message Type", base.HEX, {
     [0x0c] = "Leave",
     [0x0d] = "Connection",
     [0x0e] = "Version"
-})
+}
 
-lurk.fields = { lurk_type }
+-- for checking if the message is too short based on the length --
+-- this prevents messages being sent byte by byte to be invalidated. Must have the whole message --
+local minimum_length = {
+    [0x01] = 67, -- can be much larger than this --
+    [0x02] = 3,
+    [0x03] = 1,
+    [0x04] = 33,
+    [0x05] = 33,
+    [0x06] = 1,
+    [0x07] = 4,
+    [0x08] = 2, -- can use message_types map for the second byte --
+    [0x09] = 37,
+    [0x0a] = 48,
+    [0x0b] = 7,
+    [0x0c] = 1,
+    [0x0d] = 37,
+    [0x0e] = 5
+}
+
+-- Defining fields
+local lurk_type = ProtoField.uint8("lurk.msg_type", "Message Type", base.HEX, message_types)
+
+local accept = {
+    action = ProtoField.uint8("lurk.action", "Accepted Action", base.HEX, message_types)
+}
+
+local function handle_accept(buffer, pinfo, tree)
+    if buffer:len() < minimum_length[0x08] then
+        return false
+    end
+
+    pinfo.cols.protocol = "LURK"
+    local subtree = tree:add(lurk, buffer(), "LURK Protocol Data")
+    subtree:add(lurk_type, buffer(0, 1))
+    subtree:add(accept.action, buffer(1, 1))
+    return true
+end
+
+local function handle_small_messages(buffer, pinfo, tree)
+    pinfo.cols.protocol = "LURK"
+    local subtree = tree:add(lurk, buffer(), "LURK Protocol Data")
+    subtree:add(lurk_type, buffer(0, 1))
+end
+
+lurk.fields = {
+    lurk_type, -- all messages have this one field --
+    accept.action
+}
 
 function lurk.dissector(buffer, pinfo, tree)
     if buffer:len() < 1 then 
@@ -29,10 +76,12 @@ function lurk.dissector(buffer, pinfo, tree)
         return false
     end
 
-    pinfo.cols.protocol = "LURK"
-    local subtree = tree:add(lurk, buffer(), "LURK Protocol Data")
-    subtree:add(lurk_type, buffer(0, 1))
+    if first_byte == 0x08 then
+        return handle_accept(buffer, pinfo, tree)
+    end
 
+    -- this will be used for now since it is better than nothing for all other messages. --
+    handle_small_messages(buffer, pinfo, tree)
     return true
 end
 
